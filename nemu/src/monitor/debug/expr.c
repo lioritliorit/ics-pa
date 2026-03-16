4976d53f72c86149998d9363c49846f6ae14e618
Liorit <14295783+wang-haoransummer@user.noreply.gitee.com>
2026-03-16 21:06:13 +0800

    实现表达式求值功能

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -1,30 +1,39 @@
 #include "nemu.h"
+#include "monitor/expr.h"
 
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
-
-  /* TODO: Add more token types */
-
+  TK_NOTYPE = 256, TK_EQ, TK_NE, TK_AND, TK_OR, TK_NOT,
+  TK_NUM, TK_HEX, TK_REG,
+  TK_DEREF, TK_NEG
 };
 
 static struct rule {
   char *regex;
   int token_type;
 } rules[] = {
-
-  /* TODO: Add more rules.
-   * Pay attention to the precedence level of different rules.
-   */
-
   {" +", TK_NOTYPE},    // spaces
-  {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"0x[0-9a-fA-F]+", TK_HEX},
+  {"[0-9]+", TK_NUM},
+  {"\\$[a-zA-Z][a-zA-Z0-9]*", TK_REG},
+  {"==", TK_EQ},
+  {"!=", TK_NE},
+  {"&&", TK_AND},
+  {"\\|\\|", TK_OR},
+  {"!", TK_NOT},
+  {"\\+", '+'},
+  {"\\-", '-'},
+  {"\\*", '*'},
+  {"/", '/'},
+  {"\\(", '('},
+  {"\\)", ')'}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -53,7 +62,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-Token tokens[32];
+Token tokens[64];
 int nr_token;
 
 static bool make_token(char *e) {
@@ -74,15 +83,18 @@ static bool make_token(char *e) {
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-
-        switch (rules[i].token_type) {
-          default: TODO();
+        if (rules[i].token_type != TK_NOTYPE) {
+          if (nr_token >= 64) {
+            printf("expression too long\n");
+            return false;
+          }
+          tokens[nr_token].type = rules[i].token_type;
+          int len = substr_len;
+          if (len >= (int)sizeof(tokens[nr_token].str)) len = sizeof(tokens[nr_token].str) - 1;
+          strncpy(tokens[nr_token].str, substr_start, len);
+          tokens[nr_token].str[len] = '\0';
+          nr_token++;
         }
-
         break;
       }
     }
@@ -93,7 +105,174 @@ static bool make_token(char *e) {
     }
   }
 
+  /* Handle unary operators: negative sign and dereference. */
+  for (i = 0; i < nr_token; i ++) {
+    if (tokens[i].type == '-') {
+      if (i == 0 || (tokens[i - 1].type != TK_NUM && tokens[i - 1].type != TK_HEX &&
+                     tokens[i - 1].type != TK_REG && tokens[i - 1].type != ')')) {
+        tokens[i].type = TK_NEG;
+      }
+    }
+    else if (tokens[i].type == '*') {
+      if (i == 0 || (tokens[i - 1].type != TK_NUM && tokens[i - 1].type != TK_HEX &&
+                     tokens[i - 1].type != TK_REG && tokens[i - 1].type != ')')) {
+        tokens[i].type = TK_DEREF;
+      }
+    }
+  }
+
   return true;
+}
+
+static uint32_t reg_str2val(const char *s) {
+  int i;
+  for (i = 0; i < 8; i ++) {
+    if (strcmp(s, regsl[i]) == 0) return reg_l(i);
+    if (strcmp(s, regsw[i]) == 0) return reg_w(i);
+    if (strcmp(s, regsb[i]) == 0) return reg_b(i);
+  }
+  return 0;
+}
+
+static bool check_parentheses(int p, int q) {
+  if (tokens[p].type != '(' || tokens[q].type != ')') return false;
+
+  int cnt = 0;
+  for (int i = p; i <= q; i ++) {
+    if (tokens[i].type == '(') cnt++;
+    else if (tokens[i].type == ')') {
+      cnt--;
+      if (cnt == 0 && i < q) return false;
+    }
+  }
+  return cnt == 0;
+}
+
+static int op_precedence(int type) {
+  switch (type) {
+    case TK_OR: return 0;
+    case TK_AND: return 1;
+    case TK_EQ:
+    case TK_NE: return 2;
+    case '+':
+    case '-': return 3;
+    case '*':
+    case '/': return 4;
+    case TK_NEG:
+    case TK_DEREF:
+    case TK_NOT: return 5;
+    default: return 0;
+  }
+}
+
+static uint32_t eval(int p, int q, bool *success) {
+  if (p > q) {
+    *success = false;
+    return 0;
+  }
+
+  if (p == q) {
+    Token *t = &tokens[p];
+    uint32_t val = 0;
+    switch (t->type) {
+      case TK_NUM:
+        val = strtoul(t->str, NULL, 10);
+        break;
+      case TK_HEX:
+        val = strtoul(t->str, NULL, 16);
+        break;
+      case TK_REG:
+        val = reg_str2val(t->str + 1); /* skip '$' */
+        break;
+      default:
+        *success = false;
+        return 0;
+    }
+    *success = true;
+    return val;
+  }
+
+  if (check_parentheses(p, q)) {
+    return eval(p + 1, q - 1, success);
+  }
+
+  int min_precedence = 100;
+  int main_op = -1;
+  int parentheses = 0;
+
+  for (int i = p; i <= q; i ++) {
+    int type = tokens[i].type;
+    if (type == '(') {
+      parentheses++;
+      continue;
+    }
+    if (type == ')') {
+      parentheses--;
+      continue;
+    }
+    if (parentheses != 0) continue;
+
+    if (type == TK_NEG || type == TK_DEREF || type == TK_NOT) {
+      int prec = op_precedence(type);
+      if (prec <= min_precedence) {
+        min_precedence = prec;
+        main_op = i;
+      }
+      continue;
+    }
+
+    if (type == TK_EQ || type == TK_NE || type == TK_AND || type == TK_OR ||
+        type == '+' || type == '-' || type == '*' || type == '/') {
+      int prec = op_precedence(type);
+      if (prec <= min_precedence) {
+        min_precedence = prec;
+        main_op = i;
+      }
+    }
+  }
+
+  if (main_op == -1) {
+    *success = false;
+    return 0;
+  }
+
+  int op = tokens[main_op].type;
+  if (op == TK_NEG) {
+    uint32_t val = eval(main_op + 1, q, success);
+    return *success ? (uint32_t)(- (int32_t)val) : 0;
+  }
+  if (op == TK_DEREF) {
+    uint32_t addr = eval(main_op + 1, q, success);
+    return *success ? vaddr_read(addr, 4) : 0;
+  }
+  if (op == TK_NOT) {
+    uint32_t val = eval(main_op + 1, q, success);
+    return *success ? (val == 0 ? 1 : 0) : 0;
+  }
+
+  bool success_l, success_r;
+  uint32_t val1 = eval(p, main_op - 1, &success_l);
+  uint32_t val2 = eval(main_op + 1, q, &success_r);
+  if (!success_l || !success_r) {
+    *success = false;
+    return 0;
+  }
+
+  uint32_t result = 0;
+  switch (op) {
+    case '+': result = val1 + val2; break;
+    case '-': result = val1 - val2; break;
+    case '*': result = val1 * val2; break;
+    case '/': result = val2 == 0 ? 0 : val1 / val2; break;
+    case TK_EQ: result = (val1 == val2); break;
+    case TK_NE: result = (val1 != val2); break;
+    case TK_AND: result = (val1 && val2); break;
+    case TK_OR: result = (val1 || val2); break;
+    default: *success = false; return 0;
+  }
+
+  *success = true;
+  return result;
 }
 
 uint32_t expr(char *e, bool *success) {
@@ -102,8 +281,6 @@ uint32_t expr(char *e, bool *success) {
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  uint32_t val = eval(0, nr_token - 1, success);
+  return val;
 }
