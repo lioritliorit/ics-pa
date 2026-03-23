6532f266e0849eb658a0b49664c700d88853730f
Liorit <14295783+wang-haoransummer@user.noreply.gitee.com>
2026-03-23 16:14:16 +0800

    实现监视点功能

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -3,13 +3,20 @@
 
 #include "common.h"
 
+#define WP_EXPR_LEN 128
+
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
 
-  /* TODO: Add more members if necessary */
-
-
+  char expr[WP_EXPR_LEN];
+  uint32_t val;
 } WP;
+
+void init_wp_pool();
+WP *new_wp(const char *e);
+void free_wp(int no);
+void list_wp();
+bool check_watchpoints();
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,3 +1,5 @@
+#include "nemu.h"
+#include "monitor/monitor.h"
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
@@ -18,6 +20,90 @@ void init_wp_pool() {
   free_ = wp_pool;
 }
 
-/* TODO: Implement the functionality of watchpoint */
+WP *new_wp(const char *e) {
+  if (free_ == NULL) {
+    printf("No free watchpoint.");
+    return NULL;
+  }
+
+  WP *wp = free_;
+  free_ = free_->next;
+
+  wp->next = head;
+  head = wp;
+
+  strncpy(wp->expr, e, WP_EXPR_LEN - 1);
+  wp->expr[WP_EXPR_LEN - 1] = '\0';
+
+  bool success;
+  wp->val = expr(wp->expr, &success);
+  if (!success) {
+    printf("Invalid expression: %s\n", wp->expr);
+    free_wp(wp->NO);
+    return NULL;
+  }
+
+  return wp;
+}
+
+void free_wp(int no) {
+  WP *p = head, *prev = NULL;
+  while (p) {
+    if (p->NO == no) {
+      if (prev) {
+        prev->next = p->next;
+      } else {
+        head = p->next;
+      }
+      p->next = free_;
+      free_ = p;
+      return;
+    }
+    prev = p;
+    p = p->next;
+  }
+  printf("No watchpoint number %d\n", no);
+}
+
+void list_wp() {
+  WP *p = head;
+  if (!p) {
+    printf("No watchpoints\n");
+    return;
+  }
+
+  while (p) {
+    printf("[%d] %s = 0x%08x\n", p->NO, p->expr, p->val);
+    p = p->next;
+  }
+}
+
+bool check_watchpoints() {
+  WP *p = head;
+  bool triggered = false;
+  while (p) {
+    bool success;
+    uint32_t new_val = expr(p->expr, &success);
+    if (!success) {
+      /* Ignore evaluation errors during watchpoint checks. */
+      p = p->next;
+      continue;
+    }
+    if (new_val != p->val) {
+      printf("Watchpoint %d triggered: %s\n", p->NO, p->expr);
+      printf("  old value = 0x%08x\n", p->val);
+      printf("  new value = 0x%08x\n", new_val);
+      p->val = new_val;
+      triggered = true;
+    }
+    p = p->next;
+  }
+
+  if (triggered) {
+    nemu_state = NEMU_STOP;
+  }
+
+  return triggered;
+}
 
 
