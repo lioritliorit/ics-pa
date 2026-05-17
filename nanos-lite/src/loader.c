e8d4cbf9dceb83037ede7f00f44014b8f1dc8b6e
2311089-WangHaoran <3321265984@qq.com>
2026-05-17 16:42:26 +0800

    实现loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,14 @@
 #include "common.h"
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
 #define DEFAULT_ENTRY ((void *)0x4000000)
+#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  void *dst = DEFAULT_ENTRY;
+  void *src = &ramdisk_start;
+  memcpy(dst, src, RAMDISK_SIZE);
   return (uintptr_t)DEFAULT_ENTRY;
 }
